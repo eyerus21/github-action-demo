caa6f7fb6f5290ddb80d43b264cad96dbd656aea
eyerus21 <eyugher21@gmail.com>
2022-07-02 18:08:21 -0500

    initial commit

---
+++ src/main/java/com/java/cicd/GithubCicdActionsApplication.java
@@ -0,0 +1,20 @@
+package com.java.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+
+	@GetMapping("/api/welcome")
+	public String welcome(){
+		return "Hello World"
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
